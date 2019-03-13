9a3d4dabae5c9c551afdf40bc45fe9b8c052e1b8
Faith Chau <tszsumchau97@gmail.com>
2019-03-13 15:46:20 -0700

    hook up drafting component

--- static/src/App.js
+++ static/src/App.js
@@ -13,6 +13,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
@@ -36,16 +38,19 @@ const theme = createMuiTheme({
 
 class App extends Component {
   state = {
-    data: ""
+    anchorEl: null,
+    drafting: false
   };
 
-  componentDidMount() {
-    // axios.get("http://127.0.0.1:5000/").then(res => {
-    //   this.setState({
-    //     data: res.data
-    //   });
-    // });
-  }
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = event => {
+    console.log(event.nativeEvent.target.outerText);
+    this.setState({ anchorEl: null, drafting: true });
+  };
+
   /*
   <DraftingPage
     user={["Faith Chau", "/FaithChau.jpeg"]}
@@ -56,28 +61,56 @@ class App extends Component {
   */
 
   render() {
+    const { anchorEl } = this.state;
+
     return (
       <MuiThemeProvider theme={theme}>
         <div style={rootStyle}>
           <AppBar position="static">
             <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
+              <div>
+                <IconButton
+                  color="inherit"
+                  aria-label="Menu"
+                  onClick={this.handleClick}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Drafting</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                </Menu>
+              </div>
               <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
                 GM
               </Typography>
               <Button color="inherit">Login</Button>
             </Toolbar>
           </AppBar>
-          <div align="center">
-            <Typography variant="h1" style={titleStyle}>
-              Sign-up now to Play!
-            </Typography>
-            <Button color="secondary" variant="contained">
-              Sign-up
-            </Button>
-          </div>
+
+          {!this.state.drafting ? (
+            <div align="center">
+              <Typography variant="h1" style={titleStyle}>
+                Sign-up now to Play!
+              </Typography>
+              <Button color="secondary" variant="contained">
+                Sign-up
+              </Button>
+            </div>
+          ) : (
+            <DraftingPage
+              user={["Faith Chau", "/FaithChau.jpeg"]}
+              winRate="30%"
+              draftedPlayers={draftedPlayers}
+              playerLists={draftedPlayers}
+            />
+          )}
         </div>
       </MuiThemeProvider>
     );
