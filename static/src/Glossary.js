56e6a40e6979b95898bdd531b250882b9808909d
Faith Chau <tszsumchau97@gmail.com>
2019-04-26 18:03:45 -0700

    refactored glossary

--- static/src/Glossary.js
+++ static/src/Glossary.js
@@ -15,38 +15,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
-const gloss = {
-  Rk: "Rank",
-  Pos: "Position",
-  Age: "Age of Player\n(Feb 1st of the season)",
-  Tm: "Team",
-  G: "Games",
-  GS: "Games Started",
-  MP: "Minutes Played Per Game",
-  FG: "Field Goals Per Game",
-  FGA: "Field Goal Attempts Per Game",
-  "FG%": "Field Goal Percentage",
-  "3P": "3-Point Field Goals Per Game",
-  "3PA": "3-Point Field Goal Attempts Per Game",
-  "3P%": "FG% on 3-Pt FGAs",
-  "2P": "2-Point Field Goals Per Game",
-  "2PA": "2-Point Field Goal Attempts Per Game",
-  "2P%": "FG% on 2-Pt FGAs",
-  "eFG%":
-    "Effective Field Goal Percentage\n(3P is worth one more point than 2P)",
-  FT: "Free Throws Per Game",
-  FTA: "Free Throw Attempts Per Game",
-  "FT%": "Free Throw Percentage",
-  ORB: "Offensive Rebounds Per Game",
-  DRB: "Defensive Rebounds Per Game",
-  TRB: "Total Rebounds Per Game",
-  AST: "Assists Per Game",
-  STL: "Steals Per Game",
-  BLK: "Blocks Per Game",
-  TOV: "Turnovers Per Game",
-  PF: "Personal Fouls Per Game",
-  PTS: "Points Per Game"
-};
+import gloss from "./constants/terms.js";
 
 class Glossary extends Component {
   constructor(props) {
